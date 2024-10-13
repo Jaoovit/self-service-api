2bddafd26854bd3bfab3f5a46940a70dfc1b2b27
João Oliveira <jaoovit@gmail.com>
2024-10-13 15:55:38 +0100

    create a function to get product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,4 +15,24 @@ const getAllProducts = async (req, res) => {
   }
 };
 
-module.exports = { getAllProducts };
+const getProductById = async (req, res) => {
+  const productId = parseInt(req.params.id, 10);
+
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    res.status(200).json({
+      message: `Product ${productId} gotten sucessfully`,
+      product: product,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: `Error getting product ${productId}` });
+  }
+};
+
+module.exports = { getAllProducts, getProductById };
