dea14c34ebb5b85e11b48551b32a35c1f1fbe5aa
João Oliveira <jaoovit@gmail.com>
2024-10-13 16:10:54 +0100

    create a function do post new products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
+const { connect } = require("../routes/productRoute");
 
 const prisma = new PrismaClient();
 
@@ -18,6 +19,10 @@ const getAllProducts = async (req, res) => {
 const getProductById = async (req, res) => {
   const productId = parseInt(req.params.id, 10);
 
+  if (isNaN(postId)) {
+    res.status(400).send("Invalid product id");
+  }
+
   try {
     const product = await prisma.product.findUnique({
       where: {
@@ -35,4 +40,38 @@ const getProductById = async (req, res) => {
   }
 };
 
-module.exports = { getAllProducts, getProductById };
+const postProduct = async (req, res) => {
+  try {
+    const { title, price, description, imageUrl } = req.body;
+
+    if (!title || !price || !description || !imageUrl) {
+      return res.status(400).json({ message: "All form fields are mandatory" });
+    }
+
+    const userId = req.session.passport.user;
+
+    if (!userId) {
+      return res.status(400).json({ message: "Invalid user Id" });
+    }
+
+    const newProduct = await prisma.product.create({
+      data: {
+        title: title,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,
+        user: {
+          connect: { id: userId },
+        },
+      },
+    });
+    res
+      .status(200)
+      .json({ message: "Product created sucessfully", product: newProduct });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating new product" });
+  }
+};
+
+module.exports = { getAllProducts, getProductById, postProduct };
