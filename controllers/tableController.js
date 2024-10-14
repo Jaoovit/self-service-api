4b1493eeae18859fe7192797db807ea138745d7a
João Oliveira <jaoovit@gmail.com>
2024-10-14 18:55:24 +0100

    create a function to delete orders to a table

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -86,4 +86,107 @@ const postTable = async (req, res) => {
   }
 };
 
-module.exports = { getTablesByUser, getTableById, postTable };
+const deleteTableById = async (req, res) => {
+  const userId = parseInt(req.session.passport.user, 10);
+
+  if (!userId) {
+    return res.status(400).json({ message: "User Id Invalid" });
+  }
+
+  const tableId = parseInt(req.params.id, 10);
+
+  if (!tableId) {
+    return res.status(400).json({ message: "Invalid table Id" });
+  }
+
+  try {
+    await prisma.$transaction(async (prisma) => {
+      await prisma.order.deleteMany({
+        where: {
+          tableId: tableId,
+          userId: userId,
+        },
+      });
+
+      await prisma.table.delete({
+        where: {
+          id: tableId,
+          userId: userId,
+        },
+      });
+    });
+
+    res.status(200).json({
+      message: "Table delited successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: `Error deleting table ${tableId}` });
+  }
+};
+
+const deleteAllUserTable = async (req, res) => {
+  const userId = parseInt(req.session.passport.user);
+
+  if (!userId) {
+    return res.status(400).json({ message: "User Id Invalid" });
+  }
+
+  try {
+    await prisma.$transaction(async (prisma) => {
+      await prisma.order.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
+
+      await prisma.table.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
+    });
+
+    res.status(200).json({
+      message: "Tables delited successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting tables" });
+  }
+};
+
+const cleanUpTable = async (req, res) => {
+  const userId = parseInt(req.session.passport.user);
+
+  if (!userId) {
+    return res.status(400).json({ message: "User Id Invalid" });
+  }
+
+  const tableId = parseInt(req.params.id, 10);
+
+  if (!tableId) {
+    return res.status(400).json({ message: "Invalid table Id" });
+  }
+
+  try {
+    await prisma.table.deleteMany({
+      where: {
+        tableId: tableId,
+        userId: userId,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: `Error clening up table ${tableId}` });
+  }
+};
+
+module.exports = {
+  getTablesByUser,
+  getTableById,
+  postTable,
+  deleteTableById,
+  deleteAllUserTable,
+  cleanUpTable,
+};
