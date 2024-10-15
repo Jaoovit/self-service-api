9f3afa3a11a12ea8ebb820b110873d2858cd666c
João Oliveira <jaoovit@gmail.com>
2024-10-15 17:31:59 +0100

    create routes to controll orders

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -3,4 +3,20 @@ const orderController = require("../controllers/orderController");
 const router = express.Router();
 const verifyToken = require("../config/token");
 
+// Get
+router.get("/orders", orderController.getOrdersByUser);
+router.get("/order/:orderId", orderController.getOrdersById);
+router.get("/orders/:tableId", orderController.getOrdersByTable);
+
+// Delete
+router.delete("/order/:orderId", orderController.deleteOrderById);
+router.delete("/orders/:tableId", orderController.deleteOrderByTable);
+
+// Put
+router.put("/order/add/?orderId/:productId", orderController.addItemToOrder);
+router.put(
+  "/order/remove/?orderId/:productId",
+  orderController.removeItemFromOrder
+);
+
 module.exports = router;
