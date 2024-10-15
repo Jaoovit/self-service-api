e81f8c9c282226aad8926b64e891824daa89176b
João Oliveira <jaoovit@gmail.com>
2024-10-15 16:55:59 +0100

    create a function to add products in the order array

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -36,7 +36,7 @@ const getOrdersByTable = async (req, res) => {
 };
 
 const getOrdersById = async (req, res) => {
-  const orderId = parseInt(req.params.orderId);
+  const orderId = parseInt(req.params.orderId, 10);
 
   if (isNaN(orderId)) {
     res.status(400).send("Invalid order id");
@@ -57,8 +57,40 @@ const getOrdersById = async (req, res) => {
   }
 };
 
+const deleteOrderByTable = async (req, res) => {
+  const userId = parseInt(req.session.passport.user, 10);
+
+  if (!userId) {
+    return res.status(400).json({ message: "User Id Invalid" });
+  }
+
+  const tableId = parseInt(req.params.id, 10);
+
+  if (isNaN(tableId)) {
+    res.status(400).send("Invalid product id");
+  }
+
+  try {
+    await prisma.order.delete({
+      where: {
+        userId: userId,
+        tableId: tableId,
+      },
+    });
+
+    res.status(200).json({
+      message: `Table ${tableId} orders deleted suceessfully`,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: `Error deleting orders for table ${tableId}` });
+  }
+};
+
 const deleteOrderById = async (req, res) => {
-  const orderId = parseInt(req.params.orderId);
+  const orderId = parseInt(req.params.orderId, 10);
 
   if (isNaN(orderId)) {
     res.status(400).send("Invalid order id");
@@ -77,4 +109,50 @@ const deleteOrderById = async (req, res) => {
   }
 };
 
-module.exports = { getOrdersByTable, getOrdersById, deleteOrderById };
+const addItemToOrder = async (req, res) => {
+  const orderId = parseInt(req.query.orderId, 10);
+
+  if (isNaN(orderId)) {
+    res.status(400).send("Invalid order id");
+  }
+
+  const productId = parseInt(req.params.productId, 10);
+
+  if (isNaN(productId)) {
+    res.status(400).send("Invalid product id");
+  }
+
+  try {
+    const updatedOrder = await prisma.order.update({
+      where: {
+        id: orderId,
+      },
+      data: {
+        products: {
+          connect: { id: productId },
+        },
+      },
+      include: {
+        products: true,
+      },
+    });
+
+    return res.status(200).json({
+      message: `Product ${productId} added to the order ${orderId} successfully`,
+      order: updatedOrder,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Erro adding product ${productId} to the order ${orderId}`,
+    });
+  }
+};
+
+module.exports = {
+  getOrdersByTable,
+  getOrdersById,
+  deleteOrderByTable,
+  deleteOrderById,
+  addItemToOrder,
+};
